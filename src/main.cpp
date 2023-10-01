e2fbe3dc63f40cd5cbf7dc1ebaa07171ef920aa4
Samuel Lang <gh@lang-sam.de>
2023-10-01 20:29:34 +0200

    extend

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Thermistor.h>
 #include <NTC_Thermistor.h>
 #include <SmoothThermistor.h>
+#include <EEPROM.h>
 
 #define SENSOR_PIN A1
 #define REFERENCE_RESISTANCE 76000
@@ -10,7 +11,7 @@
 #define NOMINAL_TEMPERATURE 25
 #define B_VALUE 3950
 #define SMOOTHING_FACTOR 5
-
+#define eeAddress 10
 Thermistor *thermistor = NULL;
 
 static int serial_putc(const char c, FILE *stream) { return Serial.write(c); }
@@ -22,6 +23,21 @@ static FILE *serial_stream = fdevopen(serial_putc, NULL);
 #define FANSTUCK_THRESHOLD 500 // if no interrupts were received for 500ms, consider the fan as stuck and report 0 RPM
 // Interrupt handler. Stores the timestamps of the last 2 interrupts and handles debouncing
 unsigned long volatile ts1 = 0, ts2 = 0;
+
+#define MIN_T 22.0
+#define MIN_RPM 2000
+#define MAX_T 35.0
+#define MAX_PWM 1.0
+enum opStates
+{
+  STABILIZE_FAN,
+  CALIBRATE_FAN,
+  SCALE_FAN,
+  NO_NTC
+};
+
+enum opStates modeState;
+
 void tachISR()
 {
   unsigned long m = millis();
@@ -93,23 +109,22 @@ float getPWMpin(uint8_t pin)
   }
   return duty / 320.0;
 }
-
+static float min_pwm;
 void setup()
 {
   stdout = serial_stream;
+  Serial.begin(9600);
   pinMode(PIN_SENSE, INPUT_PULLUP);                                    // set the sense pin as input with pullup resistor
   attachInterrupt(digitalPinToInterrupt(PIN_SENSE), tachISR, FALLING); // set tachISR to be triggered when the signal on the sense pin goes low
-  Serial.begin(9600);
 
   // enable serial so we can see the RPM in the serial monitor
   // enable outputs for Timer 1
-  pinMode(9, OUTPUT);  // 1A
-  pinMode(10, OUTPUT); // 1B
+  pinMode(9, OUTPUT); // 1A
   setupTimer1();
 
-  // note that pin 11 will be unavailable for output in this mode!
-  // example...
-  setPWMpin(PIN_PWM, 0.0f); // set duty to 50% on pin 9
+  min_pwm = 0.00f;
+  EEPROM.get(eeAddress, min_pwm);
+  setPWMpin(PIN_PWM, min_pwm);
   Thermistor *originThermistor = new NTC_Thermistor(
       SENSOR_PIN,
       REFERENCE_RESISTANCE,
@@ -117,33 +132,73 @@ void setup()
       NOMINAL_TEMPERATURE,
       B_VALUE);
   thermistor = new SmoothThermistor(originThermistor, SMOOTHING_FACTOR);
+  modeState = STABILIZE_FAN;
+  delay(1500);
 }
+
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-#define MIN_T 22.0
-#define MIN_PWM 0.3
-#define MAX_T 35.0
-#define MAX_PWM 1.0
+
 void loop()
 {
   delay(200);
   auto rpm = calcRPM();
   auto pwm = getPWMpin(PIN_PWM);
+  if (100 > rpm)
+    modeState = STABILIZE_FAN;
   float celsius = thermistor->readCelsius();
 
   printf("RPM: %lu PWM: %u  T:", rpm, (uint16_t)(pwm * 100));
   Serial.println(celsius);
   // increase the duty cycle by 2% every until fan is spinning
-  if (rpm == 0)
+  if (modeState == STABILIZE_FAN)
   {
-    printf("increasing PWM: %u\n", (uint16_t)(pwm * 100));
-    setPWMpin(PIN_PWM, pwm + 0.02f);
+    if (100 > rpm)
+    {
+      printf("stabilize PWM: %u\n", (uint16_t)(pwm * 100));
+      setPWMpin(PIN_PWM, pwm + 0.1f);
+      return;
+    }
+    if (abs(rpm - MIN_RPM > 100))
+    {
+      modeState = CALIBRATE_FAN;
+      return;
+    }
+    else
+      modeState = SCALE_FAN;
     return;
   }
-
-  pwm = mapfloat(celsius, MIN_T, MAX_T, MIN_PWM, MAX_PWM);
-  setPWMpin(PIN_PWM, pwm);
+  else if (modeState = CALIBRATE_FAN)
+  {
+    if (rpm < MIN_RPM - 50)
+    {
+      printf("calibrate PWM: %u\n", (uint16_t)(pwm * 100));
+      setPWMpin(PIN_PWM, pwm + 0.01f);
+      return;
+    }
+    else if (rpm > MIN_RPM + 50)
+    {
+      printf("calibrate PWM: %u\n", (uint16_t)(pwm * 100));
+      setPWMpin(PIN_PWM, pwm - 0.01f);
+      return;
+    }
+    else
+    {
+      printf("new calibrate point PWM: %u\n", (uint16_t)(pwm * 100));
+      min_pwm = pwm;
+      EEPROM.put(eeAddress, min_pwm);
+      modeState = SCALE_FAN;
+      return;
+    }
+  }
+  else if (modeState = SCALE_FAN)
+  {
+    if (celsius < 0 or celsius > 100)
+    modeState = NO_NTC;
+    pwm = mapfloat(celsius, MIN_T, MAX_T, min_pwm, MAX_PWM);
+    pwm = constrain(pwm, min_pwm, MAX_PWM);
+    setPWMpin(PIN_PWM, pwm);
+  }
 }
-
